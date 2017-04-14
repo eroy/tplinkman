2e9fffb15c55e593634d3a0e552bef66d9caf966
Sergey <fteroy@gmial.com>
2017-04-14 23:07:53 +0300

    add constant

--- app/src/main/java/sergey/zhuravel/tplinkman/api/InfoOldService.java
+++ app/src/main/java/sergey/zhuravel/tplinkman/api/InfoOldService.java
@@ -6,53 +6,57 @@ import retrofit2.http.GET;
 import retrofit2.http.Header;
 import rx.Observable;
 import sergey.zhuravel.tplinkman.constant.ApiConstant;
+import sergey.zhuravel.tplinkman.constant.GetValueConst;
 
 
 public interface InfoOldService {
 
     @GET(ApiConstant.INFO_FIRMWARE)
-    Observable<Response<ResponseBody>> getInfoFirmware(@Header("Authorization") String cookie,
-                                                       @Header("Referer") String referer);
+    Observable<Response<ResponseBody>> getInfoFirmware(@Header(GetValueConst.AUTH) String cookie,
+                                                       @Header(GetValueConst.REFERER) String referer);
 
     @GET(ApiConstant.INFO_MAC)
-    Observable<Response<ResponseBody>> getInfoMac(@Header("Authorization") String cookie,
-                                                  @Header("Referer") String referer);
+    Observable<Response<ResponseBody>> getInfoMac(@Header(GetValueConst.AUTH) String cookie,
+                                                  @Header(GetValueConst.REFERER) String referer);
 
     @GET(ApiConstant.INFO_WIFI_NAME)
-    Observable<Response<ResponseBody>> getInfoWifiName(@Header("Authorization") String cookie,
-                                                       @Header("Referer") String referer);
+    Observable<Response<ResponseBody>> getInfoWifiName(@Header(GetValueConst.AUTH) String cookie,
+                                                       @Header(GetValueConst.REFERER) String referer);
 
     @GET(ApiConstant.INFO_WIFI_PASS)
-    Observable<Response<ResponseBody>> getInfoWifiPass(@Header("Authorization") String cookie,
-                                                       @Header("Referer") String referer);
+    Observable<Response<ResponseBody>> getInfoWifiPass(@Header(GetValueConst.AUTH) String cookie,
+                                                       @Header(GetValueConst.REFERER) String referer);
 
     @GET(ApiConstant.INFO_STATUS)
-    Observable<Response<ResponseBody>> getInfoStatus(@Header("Authorization") String cookie,
-                                                     @Header("Referer") String referer);
+    Observable<Response<ResponseBody>> getInfoStatus(@Header(GetValueConst.AUTH) String cookie,
+                                                     @Header(GetValueConst.REFERER) String referer);
 
 
     @GET(ApiConstant.INFO_WIFI_STATION)
-    Observable<Response<ResponseBody>> getInfoWifiStation(@Header("Authorization") String cookie,
-                                                          @Header("Referer") String referer);
+    Observable<Response<ResponseBody>> getInfoWifiStation(@Header(GetValueConst.AUTH) String cookie,
+                                                          @Header(GetValueConst.REFERER) String referer);
 
+    @GET(ApiConstant.INFO_WIFI_STATION_NAME)
+    Observable<Response<ResponseBody>> getInfoWifiStationName(@Header(GetValueConst.AUTH) String cookie,
+                                                              @Header(GetValueConst.REFERER) String referer);
 
     @GET(ApiConstant.INFO_WAN_DYNAMIC)
-    Observable<Response<ResponseBody>> getInfoWanDynamic(@Header("Authorization") String cookie,
-                                                         @Header("Referer") String referer);
+    Observable<Response<ResponseBody>> getInfoWanDynamic(@Header(GetValueConst.AUTH) String cookie,
+                                                         @Header(GetValueConst.REFERER) String referer);
 
     @GET(ApiConstant.INFO_WAN_STATIC)
-    Observable<Response<ResponseBody>> getInfoWanStatic(@Header("Authorization") String cookie,
-                                                        @Header("Referer") String referer);
+    Observable<Response<ResponseBody>> getInfoWanStatic(@Header(GetValueConst.AUTH) String cookie,
+                                                        @Header(GetValueConst.REFERER) String referer);
 
     @GET(ApiConstant.INFO_WAN_PPTP)
-    Observable<Response<ResponseBody>> getInfoWanPptp(@Header("Authorization") String cookie,
-                                                      @Header("Referer") String referer);
+    Observable<Response<ResponseBody>> getInfoWanPptp(@Header(GetValueConst.AUTH) String cookie,
+                                                      @Header(GetValueConst.REFERER) String referer);
 
     @GET(ApiConstant.INFO_WAN_PPPOE)
-    Observable<Response<ResponseBody>> getInfoWanPppoe(@Header("Authorization") String cookie,
-                                                       @Header("Referer") String referer);
+    Observable<Response<ResponseBody>> getInfoWanPppoe(@Header(GetValueConst.AUTH) String cookie,
+                                                       @Header(GetValueConst.REFERER) String referer);
 
     @GET(ApiConstant.INFO_WAN_TYPE)
-    Observable<Response<ResponseBody>> getInfoWanType(@Header("Authorization") String cookie,
-                                                      @Header("Referer") String referer);
+    Observable<Response<ResponseBody>> getInfoWanType(@Header(GetValueConst.AUTH) String cookie,
+                                                      @Header(GetValueConst.REFERER) String referer);
 }
--- app/src/main/java/sergey/zhuravel/tplinkman/api/InfoService.java
+++ app/src/main/java/sergey/zhuravel/tplinkman/api/InfoService.java
@@ -6,52 +6,57 @@ import retrofit2.http.GET;
 import retrofit2.http.Header;
 import rx.Observable;
 import sergey.zhuravel.tplinkman.constant.ApiConstant;
+import sergey.zhuravel.tplinkman.constant.GetValueConst;
 
 public interface InfoService {
 
     @GET(ApiConstant.INFO_FIRMWARE)
-    Observable<Response<ResponseBody>> getInfoFirmware(@Header("Cookie") String cookie,
-                                                       @Header("Referer") String referer);
+    Observable<Response<ResponseBody>> getInfoFirmware(@Header(GetValueConst.COOKIE) String cookie,
+                                                       @Header(GetValueConst.REFERER) String referer);
 
     @GET(ApiConstant.INFO_MAC)
-    Observable<Response<ResponseBody>> getInfoMac(@Header("Cookie") String cookie,
-                                                  @Header("Referer") String referer);
+    Observable<Response<ResponseBody>> getInfoMac(@Header(GetValueConst.COOKIE) String cookie,
+                                                  @Header(GetValueConst.REFERER) String referer);
 
     @GET(ApiConstant.INFO_WIFI_NAME)
-    Observable<Response<ResponseBody>> getInfoWifiName(@Header("Cookie") String cookie,
-                                                       @Header("Referer") String referer);
+    Observable<Response<ResponseBody>> getInfoWifiName(@Header(GetValueConst.COOKIE) String cookie,
+                                                       @Header(GetValueConst.REFERER) String referer);
 
     @GET(ApiConstant.INFO_WIFI_PASS)
-    Observable<Response<ResponseBody>> getInfoWifiPass(@Header("Cookie") String cookie,
-                                                       @Header("Referer") String referer);
+    Observable<Response<ResponseBody>> getInfoWifiPass(@Header(GetValueConst.COOKIE) String cookie,
+                                                       @Header(GetValueConst.REFERER) String referer);
 
     @GET(ApiConstant.INFO_STATUS)
-    Observable<Response<ResponseBody>> getInfoStatus(@Header("Cookie") String cookie,
-                                                     @Header("Referer") String referer);
+    Observable<Response<ResponseBody>> getInfoStatus(@Header(GetValueConst.COOKIE) String cookie,
+                                                     @Header(GetValueConst.REFERER) String referer);
 
 
     @GET(ApiConstant.INFO_WIFI_STATION)
-    Observable<Response<ResponseBody>> getInfoWifiStation(@Header("Cookie") String cookie,
-                                                          @Header("Referer") String referer);
+    Observable<Response<ResponseBody>> getInfoWifiStation(@Header(GetValueConst.COOKIE) String cookie,
+                                                          @Header(GetValueConst.REFERER) String referer);
+
+    @GET(ApiConstant.INFO_WIFI_STATION_NAME)
+    Observable<Response<ResponseBody>> getInfoWifiStationName(@Header(GetValueConst.COOKIE) String cookie,
+                                                              @Header(GetValueConst.REFERER) String referer);
 
     @GET(ApiConstant.INFO_WAN_DYNAMIC)
-    Observable<Response<ResponseBody>> getInfoWanDynamic(@Header("Cookie") String cookie,
-                                                         @Header("Referer") String referer);
+    Observable<Response<ResponseBody>> getInfoWanDynamic(@Header(GetValueConst.COOKIE) String cookie,
+                                                         @Header(GetValueConst.REFERER) String referer);
 
     @GET(ApiConstant.INFO_WAN_STATIC)
-    Observable<Response<ResponseBody>> getInfoWanStatic(@Header("Cookie") String cookie,
-                                                        @Header("Referer") String referer);
+    Observable<Response<ResponseBody>> getInfoWanStatic(@Header(GetValueConst.COOKIE) String cookie,
+                                                        @Header(GetValueConst.REFERER) String referer);
 
     @GET(ApiConstant.INFO_WAN_PPTP)
-    Observable<Response<ResponseBody>> getInfoWanPptp(@Header("Cookie") String cookie,
-                                                      @Header("Referer") String referer);
+    Observable<Response<ResponseBody>> getInfoWanPptp(@Header(GetValueConst.COOKIE) String cookie,
+                                                      @Header(GetValueConst.REFERER) String referer);
 
     @GET(ApiConstant.INFO_WAN_PPPOE)
-    Observable<Response<ResponseBody>> getInfoWanPppoe(@Header("Cookie") String cookie,
-                                                       @Header("Referer") String referer);
+    Observable<Response<ResponseBody>> getInfoWanPppoe(@Header(GetValueConst.COOKIE) String cookie,
+                                                       @Header(GetValueConst.REFERER) String referer);
 
     @GET(ApiConstant.INFO_WAN_TYPE)
-    Observable<Response<ResponseBody>> getInfoWanType(@Header("Cookie") String cookie,
-                                                      @Header("Referer") String referer);
+    Observable<Response<ResponseBody>> getInfoWanType(@Header(GetValueConst.COOKIE) String cookie,
+                                                      @Header(GetValueConst.REFERER) String referer);
 
 }
